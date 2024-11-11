8580e3c310a6022edb652eb2fd6a25c5c22e5d98
King_Eduardo_the_3th <mranonymous987@outlook.com>
2024-11-11 12:13:25 +0100

    Initial commit with env, app.py, and config.py

---
+++ Initiating Files/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'hamburger123'  # Set a secure key for sessions, etc.
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.btwvlgjzzlprggxkuywo:[hamburger123]@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
