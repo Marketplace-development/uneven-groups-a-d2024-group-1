f2f109bd525c679c6fece7a6ca7adb26a223e8e4
King_Eduardo_the_3th <milan.van.nuffel@outlook.com>
2024-11-20 13:18:37 +0100

    Added 3 lines in config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
 class Config:
     SECRET_KEY = 'ChairingProject123!'  # Set a secure key for sessions, etc.
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.btwvlgjzzlprggxkuywo:ChairingProject123!@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'
